f3801f037067b73cda91261e6dacfe21153c2389
Mitch24 <paulanthony1920@gmail.com>
2014-07-06 10:30:10 -0700

    Added || [] to map for officeid in RepresentativeInfoResponse to stop undefined method map for nil class error

--- lib/google_civic_info.rb
+++ lib/google_civic_info.rb
@@ -1,12 +1,12 @@
-require 'google_civic_info/representative_info_response'
-require 'google_civic_info/client'
-require 'google_civic_info/division'
-require 'google_civic_info/address'
-require 'google_civic_info/channel'
-require 'google_civic_info/office'
-require 'google_civic_info/official'
-require 'google_civic_info/phone'
-require 'google_civic_info/exceptions'
+require_relative 'google_civic_info/representative_info_response'
+require_relative 'google_civic_info/client'
+require_relative 'google_civic_info/division'
+require_relative 'google_civic_info/address'
+require_relative 'google_civic_info/channel'
+require_relative 'google_civic_info/office'
+require_relative 'google_civic_info/official'
+require_relative 'google_civic_info/phone'
+require_relative 'google_civic_info/exceptions'
 
 
 
--- lib/google_civic_info/representative_info_response.rb
+++ lib/google_civic_info/representative_info_response.rb
@@ -1,15 +1,15 @@
 module GoogleCivicInfo
   class RepresentativeInfoResponse
     attr_accessor :divisions
-  
+
     def initialize(options={})
       self.divisions = options[:divisions]
     end
-  
+
     def self.from_google_response(response)
       divisions = response['divisions'].map do |ocd_division_id, details|
         division = Division.new(:name=>details['name'], :scope=>details['scope'], :office_ids=>details['officeIds'], :ocd_division_id=>ocd_division_id)
-        division.offices = details['officeIds'].map do |office_id|
+        division.offices = (details['officeIds'] || []).map do |office_id|
           details = response['offices'][office_id]
           office = Office.new :name=>details['name'], :level=>details['level'], :official_ids=>details['officialIds']
           office.officials = details['officialIds'].map do |official_id|
@@ -17,7 +17,7 @@ module GoogleCivicInfo
             addresses = (details['address'] ||[]).map{|args| Address.new(:locationName=>args['locationName'], :line1=>args['line1'], :line2=>args['line2'], :line3=>args['line3'], :city=>args['city'], :state=>args['state'], :zip=>args['zip'])}
             channels  = (details['channels']||[]).map{|args| Channel.new(:id=>args['id'], :type=>args['type'])}
             phones    = (details['phones']  ||[]).map{|args| Phone.new(:number=>args['number'])}
-            official  = Official.new( :name=>details['name'], :phones=>phones, :emails=>details['emails'], :channels=>channels, :photoUrl=>details['photoUrl'], :urls=>details['urls'], :party=>details['party'], 
+            official  = Official.new( :name=>details['name'], :phones=>phones, :emails=>details['emails'], :channels=>channels, :photoUrl=>details['photoUrl'], :urls=>details['urls'], :party=>details['party'],
   :address=>addresses)
           end
           office
@@ -27,6 +27,6 @@ module GoogleCivicInfo
 
       self.new(:divisions=>divisions)
     end
-  
+
   end
 end
