1585ef2e342647f8511089b3cf8340b1a4b6e582
Walter McGinnis <wm@waltermcginnis.com>
2015-02-23 16:42:34 +1300

    Formatting change

--- lib/google_civic_info/division.rb
+++ lib/google_civic_info/division.rb
@@ -21,13 +21,13 @@ module GoogleCivicInfo
 
     SCOPES = [NATIONAL, STATEWIDE, CONGRESSIONAL, STATE_UPPER, STATE_LOWER,
               COUNTYWIDE, JUDICIAL, SCHOOL_BOARD, CITY_WIDE, TOWNSHIP,
-              COUNTY_COUNCIL,CITY_COUNCIL,WARD,SPECIAL]
+              COUNTY_COUNCIL, CITY_COUNCIL, WARD, SPECIAL]
 
     def initialize(options={})
       validate_inputs!(options)
       self.ocd_division_id = options[:ocd_division_id]
       self.name = options[:name]
-      self.scope = options[:scope]
+      self.scope = options[:scope] # depreciated
       self.office_indices = Array(options[:office_indices])
       self.offices = Array(options[:offices])
     end
